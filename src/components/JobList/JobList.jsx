6b4e6b30028241de8d9451ad272142d22002d5c7
Akhil Rajan <rajanakhil07@gmail.com>
2024-05-06 21:13:10 +0530

    turn off eslint validation

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import useJobs from "../../hooks/useJobs";
+import JobCard from "../JobCard/JobCard";
 
 function JobList() {
   const { jobs, loading, error } = useJobs();
@@ -8,7 +9,8 @@ function JobList() {
     <Box>
       {loading && <p> Loading jobs</p>}
       {error && <p>Error fetching jobs: {error.message}</p>}
-      {jobs.length > 0 && <pre>{JSON.stringify(jobs, null, 2)}</pre>}
+      {jobs.length > 0 &&
+        jobs.map((job) => <JobCard job={job} key={job.jdUid} />)}
     </Box>
   );
 }

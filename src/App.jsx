3fdf110217514fedd7fad4e84966bd11e53f9227
Akhil Rajan <rajanakhil07@gmail.com>
2024-05-07 18:30:09 +0530

    added store and filter slicer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import JobList from "./components/JobList/JobList";
+import store from "./store/store";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <JobList />
-    </>
+    </Provider>
   );
 }
 
